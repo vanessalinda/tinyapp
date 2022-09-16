01470c5dd5610e95b717006c15828c37d0fc998d
Vanessa Power <vanessa.l.power@gmail.com>
2022-09-16 14:29:33 -0700

    beginning creation of post request with generate randomid function and form view

--- express_server.js
+++ express_server.js
@@ -3,10 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+};
+
+const generateRandomString = () => {
+  let randomString = Math.random().toString(36).slice(2, 8);
+  return randomString;
 };
 
 app.get("/", (req, res) => {
@@ -22,10 +28,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
+  const { id } = req.params;
   const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id],
+    id,
+    longURL: urlDatabase[id],
   };
   res.render("urls_show", templateVars);
 });
