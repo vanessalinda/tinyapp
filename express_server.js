651c39095716e1110273c5384ba7772a094f313b
Vanessa Power <vanessa.l.power@gmail.com>
2022-09-17 22:04:24 -0700

    adding the cookies feature

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -24,7 +26,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -38,7 +40,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -46,6 +49,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id,
     longURL: urlDatabase[id],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -71,6 +75,20 @@ app.post("/urls/:id", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   const { id } = req.params;
   delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  //console.log(req.cookies);
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  const { username } = req.cookies;
+  //console.log(req.cookies);
+  res.clearCookie("username", username);
   res.redirect("/urls");
 });
 
